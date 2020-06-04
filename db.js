9b07a85bc97ac805a3f28a486eacb7d24670d29d
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-06-04 19:38:21 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,6 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-	PGHOST: "dumbo.db.elephantsql.com",
-	PGUSER: "fliermev",
-	PGPASSWORD: "Y3FtjUt1qvL2fHW_4lY1E8ZedtexLdGF",
-	PGDATABASE: "fliermev",
-	PGPORT: 5432,
+	
 	// PGHOST: process.env.PGHOST,
 	// PGUSER: process.env.PGUSER,
 	// PGPASSWORD: process.env.PGPASSWORD,
