a3532d7db4985c916afc547a679addbd90a1ef14
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-06-04 19:15:23 +0200

    db heroku

--- db.js
+++ db.js
@@ -1,4 +1,10 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+const pool = new Pool({
+	PGHOST: process.env.PGHOST,
+	PGUSER: process.env.PGUSER,
+	PGPASSWORD: process.env.PGPASSWORD,
+	PGDATABASE: process.env.PGDATABASE,
+	PGPORT: process.env.PGPORT,
+});
 
 module.exports = { query: (text, params) => pool.query(text, params) };
